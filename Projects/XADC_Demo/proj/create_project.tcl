ae49472e91ba47a13d9367fe08c72b38e31512e6
unknown <neastlan@WK122.ni.corp.natinst.com>
2015-11-23 16:20:11 -0800

    Projects: General: Arty_ removed from file names

--- Projects/XADC_Demo/proj/create_project.tcl
+++ Projects/XADC_Demo/proj/create_project.tcl
@@ -9,7 +9,7 @@ if {[info exists ::create_path]} {
 puts "INFO: Creating new project in $dest_dir"
 
 # Set the reference directory for source file relative paths (by default the value is script directory path)
-set proj_name "Arty_GPIO"
+set proj_name "XADC_Demo"
 
 # Set the reference directory for source file relative paths (by default the value is script directory path)
 set origin_dir ".."
